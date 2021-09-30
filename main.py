156b25c024caaedb1d0c6f7fb817a4b95b24736b
Flavian16Vali <91555258+Flavian16Vali@users.noreply.github.com>
2021-09-30 19:14:13 +0300

    am ver daca un numar este prim

--- main.py
+++ main.py
@@ -2,33 +2,43 @@
 Returneaza true daca n este prim si false daca nu.
 '''
 def is_prime(n):
-  # codul vostru aici
-  #am mai modificat ceva
+    nr=0
+    for i in range(1,n+1):
+        if n%i==0:
+            nr=nr+1
+    if nr==2:
+        return True
+    else:
+        return False
 
   
 '''
 Returneaza produsul numerelor din lista lst.
 '''
 def get_product(lst):
-  # codul vostru aici
+  print("ana are mere")
   
   
 '''
 Returneaza CMMDC a doua numere x si y folosind primul algoritm.
 '''
 def get_cmmdc_v1(x, y):
-  # codul vostru aici
-  
-  
+  print("ana are mere")
+
+
 '''
 Returneaza CMMDC a doua numere x si y folosind al doilea algoritm.
 '''
 def get_cmmdc_v2(x, y):
-  # codul vostru aici
+  print("ana are mere")
   
   
 def main():
-  # interfata de tip consola aici
+    n = int(input("n este:"))
+    if is_prime(n):
+        print("a")
+    else:
+        print("b")
 
 if __name__ == '__main__':
   main()
